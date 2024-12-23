3de77c010a926d8080854e7a029f53549e455786
Issy Long <me@issylong.com>
2024-12-23 23:56:47 +0000

    Sorbet passes at the most basic level now

--- lib/pycall-setup.rb
+++ lib/pycall-setup.rb
@@ -2,8 +2,13 @@
 
 require "pycall/import"
 
-include(Module.new do
+module InfluxDBClient3
   extend PyCall::Import
 
   pyfrom "influxdb_client_3", import: :InfluxDBClient3
-end)
+end
+
+# This was a rewrite from `include(Module.new(...))`,
+# to appease Sorbet, so let's keep the existing behaviour
+# as far as possible here and silence RuboCop.
+include InfluxDBClient3 # rubocop:disable Style/MixinUsage
