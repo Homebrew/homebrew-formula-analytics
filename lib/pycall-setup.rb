9780d547f01c24339e4bd1b998dea372c321d1d3
Issy Long <me@issylong.com>
2024-12-28 00:26:43 +0000

    If we rewrite the PyCall module import, does it work?
    - Sorbet complained about the original `Module.new(...)` syntax.
    - So I rewrote it to make a `module InfluxDBClient3` and then
      `include` it.
    - But then the actual formula-analytics command stopped
      working because it couldn't find the reference to
      `InfluxDBClient3.new`.
    - If we take a different approach here, is that any better?

--- lib/pycall-setup.rb
+++ lib/pycall-setup.rb
@@ -1,14 +1,7 @@
+# typed: false
 # frozen_string_literal: true
 
 require "pycall/import"
+include PyCall::Import # rubocop:disable Style/MixinUsage
 
-module InfluxDBClient3
-  extend PyCall::Import
-
-  pyfrom "influxdb_client_3", import: :InfluxDBClient3
-end
-
-# This was a rewrite from `include(Module.new(...))`,
-# to appease Sorbet, so let's keep the existing behaviour
-# as far as possible here and silence RuboCop.
-include InfluxDBClient3 # rubocop:disable Style/MixinUsage
+pyfrom "influxdb_client_3", import: :InfluxDBClient3
